7fa15501d95d50d0f2e697656158c2232a7c265d
Ohad Lev <ohadlev77@gmail.com>
2026-05-14 15:43:40 +0300

    Change the perturbations generation method: (1) One can pick from 3 options defined under the new Enum 'PerturbationScalingMethod'. Default is 'PerturbationScalingMethod.SCRAMBLE' (new). (2) The 'max_perturbation_locality' argument is set to be the exact locality of the generated local perturbations ($m$). We can consider to change it back to be max locality where $m$ is sampled at random from [2, max_perturbation_locality]

--- qsga/hamiltonian_generators.py
+++ qsga/hamiltonian_generators.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 from copy import deepcopy
+from enum import Enum, auto
 
 import numpy as np
+from numpy.typing import NDArray
+
 from qiskit.quantum_info import SparsePauliOp
 
 if TYPE_CHECKING:
@@ -17,9 +20,28 @@ OP11 = SparsePauliOp(data=["I", "Z"], coeffs=[0.5, -0.5]) # |1><1|
 BASIS_OPERATORS = [OP00, OP01, OP10, OP11]
 
 
+class PerturbationScalingMethod(Enum):
+    """Enum for perturbation scaling methods."""
+
+    LEFT = auto()
+    """Tensoring the local perturbation with identity operators from the left,
+    for example: XY -> IIIXY
+    """
+
+    RANDOM_LEFT_RIGHT = auto()
+    """Randomly tensor the local perturbation with identity operators from the left and right,
+    for example: XY -> IIXYI
+    """
+
+    SCRAMBLE = auto()
+    """Randomly interleaving the local perturbation with identity operators from left, right and inbetween,
+    for example: XY -> IYIXII
+    """
+
+
 def obtain_random_pauli_strings(
-    strings_length: int, # n_num_qubits
-    num_strings: int = 1, # d_regularity
+    strings_length: int, # n_num_qubits (q)
+    num_strings: int = 1, # d_regularity (d)
     basis_paulis: set[str] = {"I", "X"},
     locality: int | None = None,
     pseudo_rng: Generator = np.random.default_rng() # No predefined seed as default
@@ -161,12 +183,109 @@ def obtain_random_m_local_perturbation(
     return perturbation_hamiltonian
 
 
+def scale_m_local_perturbation(
+    m_local_perturbation: SparsePauliOp | NDArray[np.float64],
+    q_global_dim: int,
+    scaling_method: PerturbationScalingMethod,
+    simplify: bool = True,
+    pseudo_rng: Generator = np.random.default_rng() # No predefined seed as default
+) -> SparsePauliOp:
+    """Scales an m-local perturbation to act on q qubits.
+
+    Args:
+        m_local_perturbation: The m-local perturbation operator.
+        q_global_dim: The number of qubits in the global Hilbert space.
+        scaling_method: The method to use for scaling the perturbation.
+        simplify: Whether to simplify the resulting SparsePauliOp (cancel and merge identical Pauli terms).
+        pseudo_rng: Random number generator instance.
+
+    Returns:
+        SparsePauliOp: The scaled perturbation operator.
+    """
+
+    if isinstance(m_local_perturbation, np.ndarray):
+        m_local_perturbation = SparsePauliOp.from_operator(m_local_perturbation)
+    elif not isinstance(m_local_perturbation, SparsePauliOp):
+        raise ValueError("Only NumPy array or SparsePauliOp types are allowed for `m_local_perturbation`.")
+
+    m_local_dim = m_local_perturbation.num_qubits
+    scaling_dim = q_global_dim - m_local_dim
+
+    if scaling_method == PerturbationScalingMethod.LEFT:
+        scaled_perturbation = SparsePauliOp("I" * scaling_dim).tensor(m_local_perturbation)
+
+    elif scaling_method == PerturbationScalingMethod.RANDOM_LEFT_RIGHT:        
+        # NO BIAS
+        threshold = pseudo_rng.integers(scaling_dim + 1)
+
+        # EXPONENTIAL DECAY BIAS
+        # values = np.arange(scaling_dim + 1)
+        # beta = 0.75  # bigger = more biased to small indices
+        # probs = np.exp(-beta * values)
+        # probs = probs / probs.sum()
+        # probs = probs[::-1]
+
+        # print(probs)
+        # threshold = pseudo_rng.choice(values, p=probs)
+        # print(f"{threshold=}")
+
+        scaled_perturbation = SparsePauliOp("I" * threshold).tensor(m_local_perturbation).tensor(
+            SparsePauliOp("I" * (scaling_dim - threshold))
+        )
+    
+    elif scaling_method == PerturbationScalingMethod.SCRAMBLE:
+        # No bias
+        values = np.arange(q_global_dim)
+        probs = np.ones(q_global_dim) / q_global_dim
+
+        # LINEAR BIAS
+        # values = np.arange(num_qubits)
+        # probs = (num_qubits - values) / num_qubits
+        # probs = probs / sum(probs)
+
+        # POWER LAW BIAS
+        # values = np.arange(num_qubits)
+        # alpha = 3.5  # increase this for stronger bias
+        # probs = (num_qubits - values) ** alpha
+        # probs = probs / probs.sum()
+        # probs = probs[::-1]
+
+        # EXPONENTIAL DECAY BIAS
+        # values = np.arange(num_qubits)
+        # beta = 0.75  # bigger = more biased to small indices
+        # probs = np.exp(-beta * values)
+        # probs = probs / probs.sum()
+        # probs = probs[::-1]
+
+        # print(probs)
+        # print(sum(probs))
+
+        scaled_perturbation = embed_on_fixed_targets(
+            local_op=m_local_perturbation,
+            q_global_dim=q_global_dim,
+            targets=pseudo_rng.choice(
+                values,
+                size=m_local_dim,
+                p=probs,
+                replace=False
+            )
+        )
+
+    else:
+        raise ValueError("NOT ALLOWED VALUE FOR `scaling_method`.")
+    
+    if simplify:
+        scaled_perturbation = scaled_perturbation.simplify()
+
+    return scaled_perturbation
+
+
 def obtain_random_perturbed_laplacian(
-    skeleton_hamiltonian: SparsePauliOp,
-    num_perturbations: int,
-    max_perturbation_locality: int,
+    skeleton_hamiltonian: SparsePauliOp, # L_s
+    num_perturbations: int, # n_p
+    max_perturbation_locality: int, # m
     random_perturbation_weights_bounds: tuple[float, float] | None = None,
-    random_perturbations_scaling: bool = True,
+    perturbations_scaling_method: PerturbationScalingMethod = PerturbationScalingMethod.SCRAMBLE,
     simplify: bool = True,
     pseudo_rng: Generator = np.random.default_rng() # No predefined seed as default
 ) -> SparsePauliOp:
@@ -192,7 +311,10 @@ def obtain_random_perturbed_laplacian(
 
     # Adding perturbations
     for _ in range(num_perturbations):
-        perturbation_locality = pseudo_rng.integers(max_perturbation_locality) + 1
+
+        # TODO RESOLVE `m` IS MAX LOCALITY OR EXACT LOCALITY
+        # perturbation_locality = pseudo_rng.integers(max_perturbation_locality) + 1 # m = MAX LOCALITY
+        perturbation_locality = max_perturbation_locality # m = EXACT LOCALITY
 
         unscaled_perturbation = obtain_random_m_local_perturbation(
             m=perturbation_locality,
@@ -200,20 +322,47 @@ def obtain_random_perturbed_laplacian(
             simplify=simplify,
             pseudo_rng=pseudo_rng
         )
-        scaling_dim = num_qubits - perturbation_locality
 
-        # TODO CAN WE FIND A WAY TO BREAK THE PERTURBATION STRING ITSELF INTO DISTINCT PIECES?
-        if random_perturbations_scaling:
-            threshold = pseudo_rng.integers(scaling_dim + 1)
-            scaled_perturbation = SparsePauliOp("I" * threshold).tensor(unscaled_perturbation).tensor(
-                SparsePauliOp("I" * (scaling_dim - threshold))
-            )
-
-            perturbed_hamiltonian += scaled_perturbation
-        else:
-            perturbed_hamiltonian += SparsePauliOp("I" * scaling_dim).tensor(unscaled_perturbation)
+        scaled_perturbation = scale_m_local_perturbation(
+            m_local_perturbation=unscaled_perturbation,
+            q_global_dim=num_qubits,
+            scaling_method=perturbations_scaling_method,
+            simplify=simplify,
+            pseudo_rng=pseudo_rng
+        )
+        perturbed_hamiltonian += scaled_perturbation
     
     if simplify:
         perturbed_hamiltonian = perturbed_hamiltonian.simplify()
 
     return perturbed_hamiltonian
+
+
+def embed_on_fixed_targets(local_op: SparsePauliOp, q_global_dim: int, targets: list[int]):
+    """Embeds an m-local Pauli operator on q qubits on a specific set of m target qubits.
+
+    Args:
+        local_op: The m-local Pauli operator to embed.
+        q_global_dim: The number of qubits in the global Hilbert space.
+        targets: The indices of the target qubits.
+
+    Returns:
+        SparsePauliOp: The embedded Pauli operator.
+    """
+    
+    m_local_dim = len(local_op.paulis.to_labels()[0])
+    assert len(targets) == m_local_dim
+
+    new_labels, new_coeffs = [], []
+    for label, coeff in zip(local_op.paulis.to_labels(), local_op.coeffs):
+
+        big = ["I"] * q_global_dim
+        # Qiskit: rightmost char is qubit 0 (of the SMALL op)
+        for small_q in range(m_local_dim):
+            p = label[-1 - small_q]
+            big[targets[small_q]] = p
+
+        new_labels.append("".join(reversed(big)))  # back to Qiskit label order
+        new_coeffs.append(coeff)
+
+    return SparsePauliOp(new_labels, new_coeffs)
